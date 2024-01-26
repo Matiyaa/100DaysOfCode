0165ae50782e95851e2643bebb8b5887e01ea8bd
Matiyaa <40834598+Matiyaa@users.noreply.github.com>
2024-01-26 22:33:01 +0100

    Adding day 21.

---
+++ day-21-math-game.py
@@ -0,0 +1,25 @@
+from random import randint
+
+
+def main():
+    print("Welcome to the Math Game!")
+    print("You will be asked 10 questions.")
+    print("Good luck!")
+
+    score = 0
+    for i in range(0, 10):
+        num1 = randint(1, 10)
+        num2 = randint(1, 10)
+        answer = num1 + num2
+        user_answer = int(input(f"{num1} + {num2} = "))
+        if user_answer == answer:
+            score += 1
+            print("Correct!")
+        else:
+            print("Incorrect!")
+
+    print(f"You got {score} out of 10 questions correct.")
+
+
+if __name__ == '__main__':
+    main()
